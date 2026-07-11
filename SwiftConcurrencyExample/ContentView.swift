90cd3e812a4d13d6722728039490432f282bbdb3
sonaligosawi <gosawi.sonali@gmail.com>
2026-07-11 15:10:15 +0200

    Initial Commit

---
+++ SwiftConcurrencyExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftConcurrencyExample
+//
+//  Created by Sonali Gosawi on 11/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftConcurrencyExample/SwiftConcurrencyExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyExampleApp.swift
+//  SwiftConcurrencyExample
+//
+//  Created by Sonali Gosawi on 11/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
