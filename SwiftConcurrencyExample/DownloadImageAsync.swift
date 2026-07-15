c436ac76081e72f4a3e0105991260cc39f83a573
sonaligosawi <gosawi.sonali@gmail.com>
2026-07-15 16:43:44 +0200

    Added "DownloadImageAsyncImageLoader" class tand added function to  download images .

--- SwiftConcurrencyExample/DownloadImageAsync.swift
+++ SwiftConcurrencyExample/DownloadImageAsync.swift
@@ -8,31 +8,58 @@
 import SwiftUI
 internal import Combine
 
+class DownloadImageAsyncImageLoader {
+    let url = URL(string: "https://picsum.photos/200")!
+    
+    func downloadWithEscaping(completionHandler: @escaping (_ image: UIImage?, _ error: Error?) -> ()) {
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard
+                let data = data,
+                let image = UIImage(data: data),
+                let response = response as? HTTPURLResponse,
+                (200..<300).contains(response.statusCode)
+            else {
+                completionHandler(nil, error)
+                return
+            }
+            completionHandler(image, nil)
+        }
+        .resume()
+    }
+}
+
 class DownloadImageAsyncViewModel: ObservableObject {
     @Published var image: UIImage? = nil
+    let loader = DownloadImageAsyncImageLoader()
     
-    func fetchImage(){
-        self.image = UIImage(systemName:"heart.fill")
+    func fetchImage() {
+        loader.downloadWithEscaping { [weak self] image, _ in
+            DispatchQueue.main.async {
+                self?.image = image
+            }
+        }
     }
 }
 
 struct DownloadImageAsync: View {
     @StateObject var viewModel = DownloadImageAsyncViewModel()
+    
     var body: some View {
-        ZStack{
-            if let image = viewModel.image{
-                
+        ZStack {
+            if let image = viewModel.image {
                 Image(uiImage: image)
                     .resizable()
                     .scaledToFit()
                     .frame(width: 200, height: 200)
+            } else {
+                ProgressView()
             }
         }
-            .onAppear{
-                viewModel.fetchImage()
-            }
+        .onAppear {
+            viewModel.fetchImage()
         }
     }
+}
 
 #Preview {
     DownloadImageAsync()
--- SwiftConcurrencyExample/SwiftConcurrencyExampleApp.swift
+++ SwiftConcurrencyExample/SwiftConcurrencyExampleApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftConcurrencyExampleApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            DownloadImageAsync()
         }
     }
 }
