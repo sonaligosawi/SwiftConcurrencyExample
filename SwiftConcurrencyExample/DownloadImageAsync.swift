b343bc690da42eb4d23519c39cb523cabfdc8fb3
sonaligosawi <gosawi.sonali@gmail.com>
2026-07-15 15:39:53 +0200

    Added "DownloadImageAsync" view.

---
+++ SwiftConcurrencyExample/DownloadImageAsync.swift
@@ -0,0 +1,18 @@
+//
+//  DownloadImageAsync.swift
+//  SwiftConcurrencyExample
+//
+//  Created by Sonali Gosawi on 15/07/2026.
+//
+
+import SwiftUI
+
+struct DownloadImageAsync: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DownloadImageAsync()
+}
