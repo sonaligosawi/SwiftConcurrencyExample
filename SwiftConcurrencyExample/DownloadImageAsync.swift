b2c7e278d6ce133bd864eb26caf10dad299ab1b9
sonaligosawi <gosawi.sonali@gmail.com>
2026-07-15 17:29:40 +0200

    Added function to dowload image using async await.

--- SwiftConcurrencyExample/DownloadImageAsync.swift
+++ SwiftConcurrencyExample/DownloadImageAsync.swift
@@ -10,19 +10,32 @@ internal import Combine
 
 class DownloadImageAsyncImageLoader {
     let url = URL(string: "https://picsum.photos/200")!
-    
+    func handleResponse(data: Data?,response: URLResponse?)-> UIImage?{
+        guard
+            let data = data,
+            let image = UIImage(data: data),
+            let response = response as? HTTPURLResponse,
+            (200..<300).contains(response.statusCode)
+        else {
+            
+            return nil
+        }
+        return image
+    }
+    func downloadWithAsync() async throws -> UIImage?{
+        do{
+            let (data,response) = try await URLSession.shared.data(from: url, delegate: nil)
+            return handleResponse(data: data, response: response)
+            
+        }catch {
+            throw error
+        }
+        
+    }
     func downloadWithEscaping(completionHandler: @escaping (_ image: UIImage?, _ error: Error?) -> ()) {
-        URLSession.shared.dataTask(with: url) { (data, response, error) in
-            guard
-                let data = data,
-                let image = UIImage(data: data),
-                let response = response as? HTTPURLResponse,
-                (200..<300).contains(response.statusCode)
-            else {
-                completionHandler(nil, error)
-                return
-            }
-            completionHandler(image, nil)
+        URLSession.shared.dataTask(with: url) {[weak self] (data, response, error) in
+            let image = self?.handleResponse(data: data, response: response)
+            completionHandler(image, error)
         }
         .resume()
     }
@@ -32,12 +45,16 @@ class DownloadImageAsyncViewModel: ObservableObject {
     @Published var image: UIImage? = nil
     let loader = DownloadImageAsyncImageLoader()
     
-    func fetchImage() {
-        loader.downloadWithEscaping { [weak self] image, _ in
-            DispatchQueue.main.async {
-                self?.image = image
-            }
-        }
+    func fetchImage()async {
+        //        loader.downloadWithEscaping { [weak self] image, _ in
+        //            DispatchQueue.main.async {
+        //                self?.image = image
+        //            }
+        //        }
+        let image = try? await loader.downloadWithAsync()
+       await MainActor.run(body: {
+            self.image = image
+        })
     }
 }
 
@@ -56,7 +73,9 @@ struct DownloadImageAsync: View {
             }
         }
         .onAppear {
-            viewModel.fetchImage()
+            Task{
+                await viewModel.fetchImage()
+            }
         }
     }
 }
